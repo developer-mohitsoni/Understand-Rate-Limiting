826ab4c0aa907ced7a7401aba7e0dc89e45a747d
Mohit Soni <developer_mohitsoni@outlook.com>
2024-11-05 13:07:49 +0530

    Add comment to clarify rate limiting configuration

--- rate-limiting-tushar/src/config/rateLimit.ts
+++ rate-limiting-tushar/src/config/rateLimit.ts
@@ -3,7 +3,7 @@ import redis from "./redisDB";
 
 const rateLimit = new Ratelimit({
   redis: redis,
-  limiter: Ratelimit.fixedWindow(5, "1 m"),
+  limiter: Ratelimit.fixedWindow(5, "1 m"), // This will perform 5 request within 1 min.
 });
 
 export default rateLimit;
